e1a233d14c6b6ef5ff7bafb202065b5c0a949c20
Zay Yan Paing Hein <56286346+Zay-Yan@users.noreply.github.com>
2024-03-05 14:58:00 +0630

    f

--- main.tf
+++ main.tf
@@ -231,7 +231,7 @@ resource "aws_eks_node_group" "eks-cluster-ng" {
   cluster_name    = aws_eks_cluster.eks-cluster.name
   node_group_name = "EKS_NG"
   node_role_arn   = aws_iam_role.eks-cluster-ng-role.arn
-  subnet_ids      = var.eks_subnets_id
+  subnet_ids      = aws_subnet.private-subnet[*].id
 
   scaling_config {
     desired_size = 2
