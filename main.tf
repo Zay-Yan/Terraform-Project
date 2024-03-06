69269fb96ce50cacd0b1efeba8d59ce67000c22f
Zay Yan Paing Hein <56286346+Zay-Yan@users.noreply.github.com>
2024-03-06 14:01:11 +0630

    f

--- main.tf
+++ main.tf
@@ -277,13 +277,7 @@ module "eks_blueprints_addons" {
   
   }
   
-provider "helm" {
-  kubernetes {
-    host                   = aws_eks_cluster.eks-cluster.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.eks-cluster.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.eks_token.token
-  }
-}
+
 
 
 data "aws_eks_cluster_auth" "eks_token" {
