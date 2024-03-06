978bd3b38a256c19e612fcdca6bfb8374c4d4f68
Zay Yan Paing Hein <56286346+Zay-Yan@users.noreply.github.com>
2024-03-06 14:13:31 +0630

    fg

--- providers.tf
+++
@@ -1,7 +0,0 @@
-provider "helm" {
-  kubernetes {
-    host                   = aws_eks_cluster.eks-cluster.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.eks-cluster.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.eks_token.token
-  }
-}
